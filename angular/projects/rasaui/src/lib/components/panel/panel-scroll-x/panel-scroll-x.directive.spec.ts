50ab3b5852a961748cb2cfcf5d033fc1cd74e456
Victor Herrera <dsvictorh@gmail.com>
2026-07-14 10:37:16 -0600

    Add panel scroll x directive tests.

--- angular/projects/rasaui/src/lib/components/panel/panel-scroll-x/panel-scroll-x.directive.spec.ts
+++ angular/projects/rasaui/src/lib/components/panel/panel-scroll-x/panel-scroll-x.directive.spec.ts
@@ -1,8 +1,40 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
 import { PanelScrollXDirective } from './panel-scroll-x.directive';
+import { PanelComponent } from '../panel.component';
+
+@Component({
+   imports: [PanelComponent, PanelScrollXDirective],
+   template: '<rasa-panel><rasa-panel-scroll-x /></rasa-panel>',
+   encapsulation: ViewEncapsulation.None
+})
+class TestPanelComponent {}
+
+@Component({
+   imports: [PanelScrollXDirective],
+   template: '<rasa-panel-scroll-x />',
+   encapsulation: ViewEncapsulation.None
+})
+class TestNoPanelComponent {}
 
 describe('PanelScrollDirective', () => {
-   it('should create an instance', () => {
-      const directive = new PanelScrollXDirective();
-      expect(directive).toBeTruthy();
+   it('Should instantiate when in panel component', async () => {
+      await TestBed.configureTestingModule({
+         imports: [TestPanelComponent]
+      }).compileComponents();
+
+      const fixture = TestBed.createComponent(TestPanelComponent);
+      expect(fixture).toBeTruthy();
+   });
+   
+   it('Should throw error when not in panel component', async () => {
+      await TestBed.configureTestingModule({
+         imports: [TestNoPanelComponent]
+      }).compileComponents();
+
+      expect(() => TestBed.createComponent(TestNoPanelComponent)).toThrow(
+         'Directive rasa-panel-scroll-x can only be used inside component rasa-panel'
+      );
    });
 });
